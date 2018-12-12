6cd6d939b27a4907f404863c2a0fabefb2b7c036
sergiorock <lopezer.nic@gmail.com>
2018-12-11 23:58:57 -0300

    dynamic list articles

--- js/main.js
+++ js/main.js
@@ -1,9 +1,51 @@
 $(document).ready(function(){
-
+  moment.locale('es');
+  //Slider
   $('.bxslider').bxSlider({
    mode: 'fade',
    slideWidth: 1300,
    responsive: true
  });
+
+  //Posts
+  var posts = [
+    {
+      title: 'Test Article 1',
+      date: moment().format('LLL'),
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic?"
+    },
+    {
+      title: 'Test Article 2',
+      date: moment().format('LLL'),
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic?"
+    },
+    {
+      title: 'Test Article 3',
+      date: moment().format('LLL'),
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic?"
+    },
+    {
+      title: 'Test Article 4',
+      date: moment().format('LLL'),
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic?"
+    },
+    {
+      title: 'Test Article 5',
+      date: moment().format('LLL'),
+      content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus delectus incidunt blanditiis nulla asperiores necessitatibus, odio accusantium adipisci aspernatur hic?"
+    },
+  ];
+
+  posts.forEach((item, index) => {
+    var post = `
+    <article class="post">
+        <h2>${item.title}</h2>
+        <span class="date">${item.date}</span>
+        <p>${item.content}</p>
+        <a href="#" class="button-more">Leer más...</a>
+    </article>
+    `;
+    $('#posts').append(post);
+  });
 
 });
