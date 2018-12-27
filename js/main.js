32bb5c7aa3f9e663e1305f606e32f6a369bfb042
sergiorock <lopezer.nic@gmail.com>
2018-12-27 01:15:59 -0300

    add fake login on local storage

--- js/main.js
+++ js/main.js
@@ -72,4 +72,24 @@ $(document).ready(function(){
     return false;
   });
 
+  //Fake login
+  $('#login form').submit(function(){
+    var form_name = $('#form_name').val();
+    localStorage.setItem('form_name', form_name);
+  });
+
+  var form_name = localStorage.getItem('form_name');
+
+  if (form_name != null && form_name != 'undefined') {
+    var about_p = $('#about p');
+    about_p.html('<br><strong> Bienvenido ' + form_name +'</strong> ');
+    about_p.append("<a href='#' id='logout'> Cerrar sesión</a>")
+    $('#login').hide();
+
+    $('#logout').click(function(){
+      localStorage.clear();
+      location.reload();
+    });
+  };
+
 });
